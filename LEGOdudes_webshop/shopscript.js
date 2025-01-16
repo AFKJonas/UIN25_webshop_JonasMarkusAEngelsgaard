44238d62848902edcde97192450b738497278465
AFKJonas <jonasengelsgaard@gmail.com>
2025-01-16 09:06:18 +0100

    Handlekurv-LegoDudes

--- LEGOdudes_webshop/shopscript.js
+++ LEGOdudes_webshop/shopscript.js
@@ -11,8 +11,21 @@ products.map((product, index) => productHTML +=
                 <a href="#KATEGORISIDE">Ninjago</a>
                 <h3>${product.title}</h3>
                 <p>${product.price},-</p>
-                <button id="">Legg til i Handlekurv</button>
+                <button onclick="addProductToCart(${product.prodid})">Legg til i Handlekurv</button>
             </article>`)
 
 //Finn #productlist, og fyll den med verdiene i variabelen productlistHTML
 document.getElementById("productlist").innerHTML = productHTML; 
+
+//Lage toggle-funksjonalitet for handlevognen
+document.getElementById("shoppingcart").addEventListener("click", function(){
+    document.getElementById("cart").classList.toggle("visible");
+})
+
+//Lage addProductToCart()
+function addProductToCart(prodid){
+    console.log("Du vil legge til produkt med id " + prodid + " i handlekurven");
+
+    cart.push({cartprodid: prodid, quantity: 1});
+    console.log(cart);
+}
